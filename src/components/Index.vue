938408d6dbcb575a8994b1d8f0b48c4415311509
kvltkvlt <remillary@gmail.com>
2018-02-13 15:04:10 +0700

    Зачатки разумного ajax'а

---
+++ src/components/Index.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div>
+        <p>NEWS: {{content}}</p>
+        <hr>
+        <p>Date: {{date}}</p>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                content: [],
+                date: [],
+                errors: []
+            };
+        },
+
+        created() {
+            axios.get('http://localhost:7080/')
+                .then(response => {
+                    this.msg = response.data;
+                    this.content = this.msg.content;
+                    this.date = this.msg.date;
+                })
+                .catch(e => {
+                    this.errors.push(e);
+                })
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.config.productionTip = false;
+
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
